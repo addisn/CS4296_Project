4a60fef9de379bd6509d1a93e8c9d7780a30464d
Nathan Lam - desktop <kin021360@yahoo.com.hk>
2019-04-20 16:40:26 +0800

    commit static web

---
+++ StaticWebContent/js/testJson.js
@@ -0,0 +1,93 @@
+var rsp1 = {
+	"Labels" : [ {
+		"Confidence" : 99.25,
+		"Name" : "Red Light"
+	}, {
+		"Confidence" : 92.25,
+		"Name" : "Green Light"
+	}, {
+		"Confidence" : 95.24,
+		"Name" : "Yellow Light"
+	}, {
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	}, {
+		"Confidence" : 91.23,
+		"Name" : "Orange Light"
+	}, {
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},{
+		"Confidence" : 92.24,
+		"Name" : "Pink Light"
+	},]
+};
+
+var rsp2 = {
+	"Labels" : [ {
+		"Confidence" : 99.27,
+		"Name" : "Pink Flower"
+	}, {
+		"Confidence" : 99.80,
+		"Name" : "Red Flower"
+	}, ]
+
+};
+var rsp3 = {
+	"Labels" : [ {
+		"Confidence" : 99.22,
+		"Name" : "Clock"
+	}, {
+		"Confidence" : 99.25,
+		"Name" : "Pink Wall"
+	}, {
+		"Confidence" : 99.24,
+		"Name" : "Blue Wall"
+	}, ]
+
+};
